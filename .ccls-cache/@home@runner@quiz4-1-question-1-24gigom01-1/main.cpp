68bf5b5c94854841a18603e2e8a6e8f948b9a3aa
Giovanni Gomez <112590690+24gigom01@users.noreply.github.com>
2022-10-12 22:54:44 +0000

    Question 1

--- .ccls-cache/@home@runner@quiz4-1-question-1-24gigom01-1/main.cpp
+++ .ccls-cache/@home@runner@quiz4-1-question-1-24gigom01-1/main.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
   int selection;
+
+  int radius;
+  int pi;
+  int length;
+  int width;
+  int base;
+  int height;
+
   cout << "Which formula do you want to see?\n\n";
   cout << "1. Area of a circle\n";
   cout << "2. Area of a rectangle\n";
-  cout << "3. Area of a cylinder\n";
+  cout << "3. Area of a triangle\n";
   cout << "4. None of them!\n";
   cin >> selection;
- switch (selection) {
+
+  switch (selection) {
   case 1:
-    cout << "Pi times radius squared\n";
+    cout << "Enter value of pi and radius\n";
+    cin >> pi >> radius;
     break;
   case 2:
-    cout << "Length times width\n";
+    cout << "Enter value of Length and width\n";
+    cin >> length >> width;
     break;
   case 3:
-    cout << "Pi times radius squared times height\n";
+    cout << "Enter value of base and height\n";
+    cin >> base >> height;
+    break;
+  case 4:
+    cout << "Well okay then, good bye!\n";
+    break;
+  default:
+    cout << "Not good with numbers\n";
+  }
+
+  switch (selection) {
+  case 1:
+    cout << pi * radius * radius;
+    break;
+  case 2:
+    cout << length * width;
+    break;
+  case 3:
+    cout << 0.5 * base * height;
     break;
   case 4:
     cout << "Well okay then, good bye!\n";
